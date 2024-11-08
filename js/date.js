656bbc09ae18a474545eaf15e506c2b251f50429
ReMaxz <151454937+ReMaxzz@users.noreply.github.com>
2024-11-09 04:08:38 +0700

    Add files via upload

---
+++ js/date.js
@@ -0,0 +1,34 @@
+
+var dv = document.getElementById("content");
+dv.style.opacity = 0;
+var val = 0;
+
+function timer(){
+	var m = "";
+	document.getElementById("m").innerHTML = m;
+}
+
+function fadein(){
+	if(val < 1){
+		val += 0.025;
+		dv.style.opacity = val;
+	}
+	else{
+		clearInterval(fadeinInterval);
+		if(ok == 2){
+			ok += 1;
+		}
+	}
+}
+
+var fadeInterval;
+var fadeinInterval;
+
+timer();
+setInterval(timer, 1000);
+fadeInterval = setInterval(function(){
+	if(ok == 2){
+		clearInterval(fadeInterval);
+		fadeinInterval = setInterval(fadein, 50);
+	}
+}, 50)
---
+++ js/playImg.js
@@ -0,0 +1,94 @@
+
+var btn = document.getElementById("heartTxt");
+btn.style.opacity = 0;
+var btnVal = 0;
+
+function showImage(){
+	//document.getElementById("imgTxt").style.opacity = 0;
+	myImage.setAttribute("src", imageArray[imageIndex]);
+	myTxt.innerHTML = txtArray[imageIndex];
+	//document.getElementById("imgTxt").style.opacity = 1 - flag;
+	imageIndex++;
+	if(imageIndex >= len){
+		imageIndex = 0;
+	}
+}
+
+let clickCount = 0;
+  
+function handleClick() {
+  clickCount++;
+
+  if (clickCount === 1) {
+	// กดครั้งแรก เล่นเสียง
+	play();
+  } else if (clickCount === 2) {
+	// กดครั้งที่สอง นำไปสู่หน้าใหม่
+	goToWishes();
+  }
+}
+
+function play() {
+	if(t == 0){
+		myImage.setAttribute("src", "");
+		myTxt.innerHTML = "";
+		imageIndex = 0;
+		clearInterval(showImageInterval);
+	}
+	flag = 1 - flag;
+	document.getElementById("typeDiv").style.opacity = flag;
+	document.getElementById("imgTxt").style.opacity = 1 - flag;
+	if(t == 0){
+		//setTimeout(showImage, 1000);
+		setInterval(showImage, 2500);
+	}
+	t++;
+}
+
+function goToWishes() {
+  window.location.href = 'birthday-wishes.html';
+}
+
+
+function preshowImage(){
+	document.getElementById("imgTxt").style.opacity = 0;
+	myImage.setAttribute("src", imageArray[imageIndex]);
+	myTxt.innerHTML = txtArray[imageIndex];
+	imageIndex++;
+	if(imageIndex >= len){
+		imageIndex = 0;
+	}
+}
+
+function buttonFadeIn(){
+	if(btnVal < 1){
+		btnVal += 0.025;
+		btn.style.opacity = btnVal;
+	}
+	else{
+		clearInterval(buttonInterval);
+		if(ok == 3){
+			ok += 1;
+		}
+	}
+}
+
+
+
+function event(){
+
+	showImageInterval = setInterval(preshowImage, 100);
+
+	imgInterval = setInterval(function (){
+		if(ok == 3){
+			setTimeout(function(){buttonInterval = setInterval(buttonFadeIn, 50);}, 1500);
+			clearInterval(imgInterval);
+		}
+	}, 50);
+}
+
+var showImageInterval;
+var imgInterval;
+var buttonInterval;
+
+event();
